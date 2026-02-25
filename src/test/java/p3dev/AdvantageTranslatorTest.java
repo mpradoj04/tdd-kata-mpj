e210b9415360433c06f1509c9b2454d024429827
mpradoj04 <mprajim860@gmail.com>
2026-02-25 17:30:07 +0100

    fix: Fixed scope of class to only advantage

--- src/test/java/p3dev/AdvantageTranslatorTest.java
+++ src/test/java/p3dev/AdvantageTranslatorTest.java
@@ -8,20 +8,12 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
 import java.util.stream.Stream;
 import org.junit.jupiter.params.provider.Arguments;
 
-public class AdvantageAndVictoryTranslatorTest {
+public class AdvantageTranslatorTest {
 
     static Stream<Arguments> advantageAndVictoryProvider() {
         return Stream.of(
                 arguments(4, 3, "Advantage Player 1"),
-                arguments(3, 4, "Advantage Player 2"),
-                arguments(5, 3, "Player 1 wins"),
-                arguments(5, 2, "Player 1 wins"),
-                arguments(5, 1, "Player 1 wins"),
-                arguments(5, 0, "Player 1 wins"),
-                arguments(3, 5, "Player 2 wins"),
-                arguments(2, 5, "Player 2 wins"),
-                arguments(1, 5, "Player 2 wins"),
-                arguments(0, 5, "Player 2 wins")
+                arguments(3, 4, "Advantage Player 2")
         );
     }
 
@@ -32,6 +24,8 @@ public class AdvantageAndVictoryTranslatorTest {
                 arguments(2, 2),
                 arguments(3, 3),
                 arguments(4, 4),
+                arguments(5, 2),
+                arguments(1, 5),
                 arguments(1, 3),
                 arguments(1, 4),
                 arguments(2, 3),
@@ -47,7 +41,7 @@ public class AdvantageAndVictoryTranslatorTest {
     @MethodSource("advantageAndVictoryProvider")
     void should_return_correct_string_for_correct_advantage_and_victory_scores(int p1Points, int p2Points, String expected) {
         // Arrange
-        AdvantageAndVictoryTranslator translator = new AdvantageAndVictoryTranslator();
+        AdvantageTranslator translator = new AdvantageTranslator();
 
         // Act
         String result = translator.translate(p1Points, p2Points);
@@ -60,7 +54,7 @@ public class AdvantageAndVictoryTranslatorTest {
     @MethodSource("wrongAdvantageAndVictoryProvider")
     void should_throw_exception_for_incorrect_advantage_and_victory_scores(int p1Points, int p2Points) {
         // Arrange
-        AdvantageAndVictoryTranslator translator = new AdvantageAndVictoryTranslator();
+        AdvantageTranslator translator = new AdvantageTranslator();
 
         // Assert
         assertThrows(IllegalArgumentException.class, () -> translator.translate(p1Points, p2Points));
