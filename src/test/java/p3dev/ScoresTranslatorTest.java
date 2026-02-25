dd40e1105620e1e5e2450b427cac54345f7b96da
mpradoj04 <mprajim860@gmail.com>
2026-02-25 17:58:46 +0100

    fix: Added negative test cases

--- src/test/java/p3dev/ScoresTranslatorTest.java
+++ src/test/java/p3dev/ScoresTranslatorTest.java
@@ -4,8 +4,9 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class ScoreTranslatorTest {
+public class ScoresTranslatorTest {
 
     static Stream<TennisTestCase> testCasesProvider() {
         return Stream.of(
@@ -28,16 +29,35 @@ public class ScoreTranslatorTest {
         );
     }
 
+    static Stream<InvalidScoreCase> invalidScoresProvider() {
+        return Stream.of(
+                new InvalidScoreCase(-1, 0),
+                new InvalidScoreCase(0, -5),
+                new InvalidScoreCase(10, 2),
+                new InvalidScoreCase(2, 10)
+        );
+    }
+
     @ParameterizedTest
     @MethodSource("testCasesProvider")
     void should_return_expected_tennis_score(TennisTestCase testCase) {
-        TennisGameTranslator translator = new TennisGameTranslator();
+        ScoreTranslator translator = new ScoreTranslator();
 
         // Act
         String result = translator.translate(testCase.p1, testCase.p2);
 
         // Assert
         assertEquals(testCase.expectedResult, result);
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidScoresProvider")
+    void should_throw_exception_for_unhandled_scores(InvalidScoreCase invalidCase) {
+        ScoreTranslator translator = new ScoreTranslator();
+
+        assertThrows(IllegalStateException.class, () ->
+                translator.translate(invalidCase.p1, invalidCase.p2)
+        );
     }
 
     static class TennisTestCase {
@@ -55,5 +75,14 @@ public class ScoreTranslatorTest {
         public String toString() {
             return String.format("Score %d-%d should return \"%s\"", p1, p2, expectedResult);
         }
+    }
+
+    static class InvalidScoreCase {
+        int p1, p2;
+        InvalidScoreCase(int p1, int p2) {
+            this.p1 = p1; this.p2 = p2;
+        }
+        @Override
+        public String toString() { return String.format("%d-%d", p1, p2); }
     }
 }
